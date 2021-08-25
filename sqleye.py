c195663d2d4b5fb57f2aa117c4fe7c511f969322
EYE zik <eyezikbra@gmail.com>
2021-08-25 04:38:08 -0700

    Update sqleye.py

--- sqleye.py
+++ sqleye.py
@@ -18,21 +18,25 @@ def presentation():
 def gethref(ip):
     url = ("http://" + ip)
     print("[x] ~ SCAN: " + url + " ~ [x]")
-    req = requests.get(url)
+    req = requests.get(url, timeout=6)
     soup = BeautifulSoup(req.text, 'html.parser')
     for link in soup.select('a[href*=".php?id="]'):
         okay = (link["href"])
         serv = (url + "/" + okay + "'")
-        reeqee = requests.get(serv)
-        souper = BeautifulSoup(reeqee.text, "html.parser")
-        if souper(text=lambda t: "SQL syntax" in t):
-            print(serv + " :  [!] VULN [!]")
-            fo = open("vulnSQLi.txt", "a+")
-            fo.write(serv + "\n")
-            fo.close
-        else:
-            print("[x] found sqli but no pass [x] : " + serv )
-			
+        try:
+            reeqee = requests.get(serv, timeout=6)
+            souper = BeautifulSoup(reeqee.text, "html.parser")
+            if souper(text=lambda t: "SQL syntax" in t):
+                print(serv + " :  [!] VULN [!]")
+                fo = open("vulnSQLi.txt", "a+")
+                fo.write(serv + "\n")
+               fo.close
+            else:
+                print("[x] found sqli but no pass [x] : " + serv )
+			    pass
+        except:
+            pass
+        
 			
 
 def main():
@@ -46,7 +50,7 @@ def main():
 		input_file = open(sys.argv[2])
 		#threads = (sys.argv[3])
 		for i in input_file.readlines():
-		ip = i.strip("\n")
-		gethref(ip)
+			ip = i.strip("\n")
+			gethref(ip)
 
 main()
